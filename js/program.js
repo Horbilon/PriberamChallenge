a9e5c9a641574e64b318c981a11b84c06f078d37
Horbilon <goncalos_lacerda@hotmail.com>
2022-01-21 09:14:54 +0000

    JSON connection

--- js/program.js
+++ js/program.js
@@ -1,10 +1,20 @@
-//let jsonFile = require('../js/entries.json');
+//let jsonFile = fetch('../js/entries.json');
+//let jsonFile = JSON.parse(fs.readFileSync('../js/entries.json').toString());
+var json = (function() {
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "../js/entries.json",
+        'dataType': "json",
+        'success': function(data) {
+            json = data;
+        }
+    });
+    return json;
+})();
 
-let workTag={
-    text:"",
-    tags:[]
-};
-
+//ArrayList para guardar temporariamente as posições que estão na tabela
 let workTagArrayList=[];
 
 $(function() {
@@ -18,82 +28,66 @@ $(function() {
         let end = $(this)[0].selectionEnd;
         if(end-start!=0){
             placeholder = placeholder.substr(start, end - start);
-
-            console.log('Start:'+start+'End:'+ end)
-            console.log(placeholder)
+            console.log('Start:'+start+'End:'+ end);
+            console.log(placeholder);
             if (placeholder!=null) {
                 if(workTagArrayList.length==null){
                      placeholderID = 0;
-                } else {  placeholderID=workTagArrayList.length}
+                } else {
+                    placeholderID=workTagArrayList.length;
+                };
                 document.getElementById("table-body").innerHTML+= build(placeholder, placeholderID);
                 let workTagArray = {
                     id:placeholderID,
                     indexStart:start,
                     indexEnd:end,
-                    tag:""
-                }
+                    TRtag:""
+                };
                 workTagArrayList.push(workTagArray);
             };
         };
         console.log(workTagArrayList)
-
     });
 });
 
-function build (base, id) {
-    return `<tr>
-                <th scope="row" id="${id}">${id}</th>                                        
-                    <td>${base}</td>
-                    <td>
-                        <select id="inputState">
-                            <option selected>Tags</option>
-                            <option>Place</option>
-                            <option>Organization</option>
-                            <option>Place</option>
-                            <option>Event</option>
-                        </select>
-                    </td>
-                    <td>
-                    <a  class="btn btn-danger">Delete</a> 
-                    <button type="submit" class="btn btn-success">Save</button>                   
+function build (text, id) {
+    //id is being passed to that, if needed, a entry can be deleted.
+    return `<tr id="${id}">                                                     
+                <td>${text}</td>
+                <td>
+                    <select id="inputState">
+                        <option selected>Tags</option>
+                        <option value="Person">Person</option>
+                        <option value="Organization">Organization</option>
+                        <option value="Place">Place</option>
+                        <option value="Event">Event</option>
+                    </select>
+                </td>
+                <td>
+                    <a onclick="remove(${id})" class="btn btn-danger">Delete</a> 
+                                      
                 </td>
             </tr>
     `;
 }
 
+function saveProgess() {
+    let IDEntry = json.length;
+    console.log(IDEntry)
+}
+
+function remove(id) {
+    document.getElementById(id).remove();
+    delete workTagArrayList[id];
+}
 
 function goStartNew() {
     let readText = document.getElementById("getText").value;
-    console.log(readText);
-
-    //document.getElementById("program").innerHTML=insertElement();
     document.getElementById("read").innerText+=`${readText}`
-    document.getElementById("forTable").innerHTML=createTable();
+
 }
 
 function goContinue() {
 let id = document.getElementById("id").value;
 }
 
-function insertElement(){
-    return `<div class="col input-group">
-                <textarea class="form-control" id="read"></textarea>
-            </div>
-            <div id="forTable">
-            </div>`;
-}
-
-function createTable() {
-    return `<table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Text</th>                
-                <th scope="col">Tag</th>
-                <th scope="col">Other</th>
-              </tr>
-            </thead>
-            <tbody id="table-body">
-            </tbody>
-          </table>`
-}
